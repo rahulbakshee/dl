8ccf237c54c6a494aeae2c07d91a981437c95941
Rahul Bakshee <rhlbks@gmail.com>
2017-03-04 02:54:15 +0530

    Add files via upload

---
+++ Simple_ANN_1_hidden_layer.py
@@ -0,0 +1,35 @@
+################################################################################################
+# Below code is highly inspired from http://iamtrask.github.io/2015/07/12/basic-python-network/#
+################################################################################################
+
+
+import numpy as np
+
+
+#input and output
+x = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1] ])
+y = np.array([[0,1,1,0]]).T
+
+
+#weights initialisation
+syn0 = 2*np.random.random((3,4)) - 1
+syn1 = 2*np.random.random((4,1)) - 1
+
+
+for i in range(100000):
+    #forward
+    l1 = 1/(1 + np.exp(-(np.dot(x,syn0))))
+    l2 = 1/(1 + np.exp(-(np.dot(l1, syn1))))
+    #print("l1", l1)
+    #print("l2", l2)
+
+    #backprop
+    l2_delta = (y - l2)*(l2*(1-l2))
+    l1_delta = l2_delta.dot(syn1.T)*(l1*(1-l1))
+
+    #weights update
+    syn1 += l1.T.dot(l2_delta)
+    syn0 += x.T.dot(l1_delta)
+    if i%10000 == 0:
+        print("l2 error mean", y-l2)
+print("l2",l2)
